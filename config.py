f3d1d13670d8d8981b182f4e6bb7ec3a24557a7a
Bunny <Cy3er3unny.Official@gmail.com>
2025-04-29 19:35:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class Config:
-    API_ID = 
-    API_HASH = ""
-    TOKEN = ""  
+    API_ID = "14853951"
+    API_HASH = "0a33bc287078d4dace12aaecc8e73545"
+    TOKEN = "7850386310:AAFHTXecLRW9AdVwBLGKlqe6aZlBZckMFvs"  
     START_PIC = "" 
     CHAT = ""    
