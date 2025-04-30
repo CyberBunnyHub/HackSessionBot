85cc4feaf41319619459cac3f6717484b9cd5f10
Bunny <Cy3er3unny.Official@gmail.com>
2025-04-30 13:57:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class Config:
     API_ID = "14853951"
     API_HASH = "0a33bc287078d4dace12aaecc8e73545"
-    TOKEN = "7850386310:AAFHTXecLRW9AdVwBLGKlqe6aZlBZckMFvs"  
+    TOKEN = "8031351073:AAHVSWeqFGKRhygt71l8MxJzwRPq9JxVhLQ"  
     START_PIC = "https://i.ibb.co/dJw7sbCh/5a5b38ed84fb.jpg" 
     CHAT = "https://t.me/+AHzGKHFZH_hlMWE9"    
