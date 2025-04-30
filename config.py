3d1a6628059542c3f1e37d486ee38f3d97fd74c6
Bunny <Cy3er3unny.Official@gmail.com>
2025-04-30 09:30:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ class Config:
     TOKEN = "7850386310:AAFHTXecLRW9AdVwBLGKlqe6aZlBZckMFvs"  
     START_PIC = "https://i.ibb.co/dJw7sbCh/5a5b38ed84fb.jpg" 
     CHAT = "https://t.me/+AHzGKHFZH_hlMWE9"    
-    PORT = "8080"
+    PORT = "1987"
     
